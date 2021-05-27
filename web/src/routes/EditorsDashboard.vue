967aa838fd388a7daa30728bd72c2c7184bb78e5
Nerixyz <nero.9@hotmail.de>
2021-05-28 01:02:48 +0200

    refactor(web): transform EditorsDashboard to use cards

--- web/src/routes/EditorsDashboard.vue
+++ web/src/routes/EditorsDashboard.vue
@@ -1,18 +1,35 @@
 <template>
   <div class="px-20 pt-5">
-    <div v-if="loading">Loading...</div>
-    <div v-else-if="error">
-      Failed!
-      <br />
-      <pre>{{ error }}</pre>
-    </div>
-
-    <div v-else class="flex gap-10">
-      <form class="w-1/6 min-w-10rem" @submit="addEditor">
-        <h3 class="mb-3">Add an editor</h3>
-        <TextField v-model="editorAddName" label="Name" :disabled="loading" />
+    <div class="flex gap-5 flex-col">
+      <form class="w-full min-w-10rem border-b border-gray-900 border-opacity-20 pb-6" @submit="addEditor">
+        <h3 class="ml-1 mb-3 font-serif text-xl">Add an editor</h3>
+        <div class="flex w-full items-center">
+          <TextField class="flex-grow" v-model="editorAddName" label="Name" :disabled="loading" />
+          <button
+            type="submit"
+            class="
+              mx-3
+              px-3
+              py-3
+              rounded-full
+              bg-gray-350 bg-opacity-40
+              hover:bg-gray-500 hover:bg-opacity-40
+              transition transition-color
+            "
+          >
+            <PlaneIcon />
+          </button>
+        </div>
       </form>
-      <div class="flex-grow">
+
+      <div v-if="loading">Loading...</div>
+      <div v-else-if="error">
+        Failed!
+        <br />
+        <pre>{{ error }}</pre>
+        <OutlinedButton>Ok</OutlinedButton>
+      </div>
+      <div v-else class="">
         <div v-if="!editors.length">
           No editors? Seriously? Let your mods do the work, add some editors!
           <img
@@ -26,16 +43,51 @@
             alt="ModTime"
           />
         </div>
-        <div v-for="editor of editors" v-else :key="editor.id" class="flex items-center gap-4">
-          <img
-            :src="editor.profile_image_url"
-            :alt="`Profile image of ${editor.login}`"
-            class="w-10 h-10 rounded-full"
-          />
-          <h3>{{ editor.login }}</h3>
-          <OutlinedButton :disabled="loading" class="ml-auto" @click="removeEditor(editor.login)"
-            >Remove</OutlinedButton
+        <div v-else class="flex flex-wrap gap-5">
+          <div
+            v-for="editor of editors"
+            :key="editor.id"
+            class="
+              flex
+              items-center
+              flex-col
+              gap-4
+              bg-gray-300
+              rounded-lg
+              overflow-hidden
+              border border-gray-900 border-opacity-30
+              pt-4
+              select-none
+            "
+            @click="removeEditor(editor.login)"
           >
+            <img
+              :src="editor.profile_image_url"
+              :alt="`Profile image of ${editor.login}`"
+              class="w-10 h-10 rounded-full"
+            />
+            <h3>{{ editor.login }}</h3>
+            <button
+              class="
+                bg-red
+                text-black
+                font-bold
+                uppercase
+                w-full
+                px-7
+                py-2
+                transition transition-colors
+                hover:bg-transparent
+                hover:text-red
+                focus:bg-transparent
+                focus:text-red
+                focus:outline-none
+              "
+              @click="removeEditor(editor.login)"
+            >
+              Remove
+            </button>
+          </div>
         </div>
       </div>
     </div>
@@ -49,10 +101,11 @@ import { asyncRefs, tryAsync } from '../utilities';
 import TextField from '../components/core/TextField.vue';
 import OutlinedButton from '../components/core/OutlinedButton.vue';
 import { TwitchUser } from '../api/types';
+import PlaneIcon from '../components/icons/PlaneIcon.vue';
 
 export default defineComponent({
   name: 'EditorsDashboard',
-  components: { TextField, OutlinedButton },
+  components: { PlaneIcon, TextField, OutlinedButton },
 
   setup() {
     const api = useApi();
