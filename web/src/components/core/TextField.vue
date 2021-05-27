5066890efde9b137aa47b8b1646cb596d7882db4
Nerixyz <nero.9@hotmail.de>
2021-05-27 23:45:25 +0200

    feat(web): make the textfield cooler 😎

--- web/src/components/core/TextField.vue
+++ web/src/components/core/TextField.vue
@@ -1,60 +1,71 @@
 <template>
-  <div
-    class="
-      bg-transparent
-      rounded-t-lg rounded-b-sm
-      border-b-2 border-red
+  <div class="block">
+    <div
+      :class="[
+        `bg-transparent
+      rounded-t-lg
+      border border-gray-900
+      border-opacity-30
       transition-colors
       hover:bg-gray-350
-      focus:bg-gray-500
-      relative
-    "
-  >
-    <span
-      class="
-        transform
-        -translate-y-3
-        scale-75
-        text-pink-600
+      relative nerix-underline-tf`,
+        isFocused ? 'nerix-underline-tf-active bg-gray-350' : '',
+      ]"
+    >
+      <span
+        :class="[
+          isOccupied
+            ? `-translate-y-3
+        scale-75`
+            : '',
+          isFocused ? 'text-red' : 'text-gray-700',
+          `transform
+        transition-transform
+        transition-colors
+        duration-200
+        ease-cubic-out
         absolute
-        left-1
+        left-2
         right-auto
         max-w-full
         overflow-hidden overflow-ellipsis
         whitespace-nowrap
         pointer-events-none
         origin-top-left
-        top-3
-      "
-    >
-      {{ label }}
-    </span>
-    <WarnIcon
-      v-if="warn"
-      class="
-        text-yellow-400
-        absolute
-        left-auto
-        right-1
-        max-w-full
-        overflow-hidden overflow-ellipsis
-        whitespace-nowrap
-        pointer-events-none
-        origin-top-left
-        top-5
-      "
-    />
-    <input
-      :value="modelValue"
-      :disabled="disabled"
-      class="bg-transparent w-full h-full px-3 py-2 border-none mt-2 outline-none"
-      @input="onInput"
-    />
+        top-3`,
+        ]"
+      >
+        {{ label }}
+      </span>
+      <WarnIcon
+        v-if="warn"
+        class="
+          text-yellow-400
+          absolute
+          left-auto
+          right-2
+          max-w-full
+          overflow-hidden overflow-ellipsis
+          whitespace-nowrap
+          pointer-events-none
+          origin-top-left
+          top-4
+        "
+      />
+      <input
+        :value="modelValue"
+        :disabled="disabled"
+        class="bg-transparent w-full h-full px-3 py-2 border-none mt-2 outline-none"
+        @input="onInput"
+        @focus="onFocus"
+        @blur="onBlur"
+      />
+    </div>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { computed, defineComponent, ref, toRefs } from 'vue';
 import WarnIcon from '../icons/WarnIcon.vue';
 
 export default defineComponent({
@@ -85,13 +96,47 @@ export default defineComponent({
     },
   },
   emits: ['update:modelValue'],
-  setup(_props, { emit }) {
+  setup(props, { emit }) {
+    const { modelValue } = toRefs(props);
+
     const onInput = (e: Event) => {
       // eslint-disable-next-line @typescript-eslint/no-explicit-any
       emit('update:modelValue', (e.target as any).value);
     };
 
-    return { onInput };
+    const isFocused = ref(false);
+    const onFocus = () => {
+      console.log('focus');
+      isFocused.value = true;
+    };
+    const onBlur = () => {
+      console.log('blur');
+      isFocused.value = false;
+    };
+
+    const isOccupied = computed(() => isFocused.value || modelValue.value);
+
+    return { onInput, isOccupied, onFocus, onBlur, isFocused };
   },
 });
 </script>
+<style scoped>
+.nerix-underline-tf::after {
+  content: '';
+  position: relative;
+  display: block;
+  width: 100%;
+  height: 0.1rem;
+  background-color: #ff4151;
+  transform: scaleX(0);
+  transition: transform 150ms;
+  transform-origin: right;
+  top: 0.05rem;
+}
+
+.nerix-underline-tf:hover::after,
+.nerix-underline-tf-active::after {
+  transform: scaleX(1);
+  transform-origin: left;
+}
+</style>
