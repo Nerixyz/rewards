de83ebf954e1523436a4d1724737ce5faef6d150
Nerixyz <nero.9@hotmail.de>
2021-05-31 17:26:00 +0200

    feat(web): properly add CSlider

--- web/src/components/core/CSlider.vue
+++ web/src/components/core/CSlider.vue
@@ -1,13 +1,55 @@
 <template>
-  <input type="range" />
+  <div class="w-full mb-2">
+    <span>{{ label }}</span>
+    <div class="flex w-full gap-3 px-1">
+      <input class='flex-grow' type="range" :value="value" :min="min" :max="max" :step="step" @input="onUpdate" />
+      <span>{{ currentValueStr }}</span>
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { computed, defineComponent, ref, toRefs, watch } from 'vue';
 
 export default defineComponent({
   name: 'CSlider',
+  props: {
+    label: {
+      type: String,
+      required: true,
+    },
+    modelValue: {
+      type: Number,
+      required: true,
+    },
+    min: {
+      type: Number,
+      required: false,
+      default: 0,
+    },
+    max: {
+      type: Number,
+      required: false,
+      default: 100,
+    },
+    step: {
+      type: Number,
+      required: false,
+      default: 1,
+    },
+  },
+  emits: ['update:modelValue'],
+  setup(props, { emit }) {
+    const { modelValue, min, step } = toRefs(props);
+    const value = ref(modelValue.value);
+    watch(modelValue, v => {
+      value.value = v;
+    });
+    const onUpdate = (e: Event) => {
+      emit('update:modelValue', Number((e.target as any).value) || min.value);
+    };
+    const currentValueStr = computed(() => (Math.round(value.value / step.value) * step.value).toString());
+    return { value, onUpdate, currentValueStr };
+  },
 });
 </script>
-
-<style scoped></style>
