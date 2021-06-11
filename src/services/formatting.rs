e155aa8941c71eb4b41651c09db4a50a3d6ff234
Nerixyz <nero.9@hotmail.de>
2021-06-11 15:32:45 +0200

    fix: update human formatter

--- src/services/formatting.rs
+++ src/services/formatting.rs
@@ -2,14 +2,13 @@ const S_IN_MNTH: u64 = 2628003; // 2628002,88 seconds according to Google
 
 /// This is basically the Short formatting if [`timeago`](https://docs.rs/timeago/0.0.2/src/timeago/lib.rs.html#24-98) without the "ago".
 pub fn human_format_duration(duration: &chrono::Duration) -> String {
-    let secs = duration.num_seconds();
-    if secs > 0 {
-        format!("{} ago", format_secs_short(secs as u64))
-    } else if secs < 0 {
-        let actual_secs = (-secs) as u64;
-        format!("in {}", format_secs_short(actual_secs))
-    } else {
-        "now".into()
+    match duration.num_seconds() {
+        secs if secs > 0 => format!("{} ago", format_secs_short(secs as u64)),
+        secs if secs < 0 => {
+            let actual_secs = (-secs) as u64;
+            format!("in {}", format_secs_short(actual_secs))
+        }
+        _ => "now".into(),
     }
 }
 
@@ -18,19 +17,19 @@ fn format_secs_short(secs: u64) -> String {
         0 => "0 seconds".into(), // never
         1 => "1 second".into(),
         x if x > 1 && x < 60 => format!("{} seconds", x),
-        x if x >= 60 && x < 120 => "1 minute".into(),
-        x if x >= 120 && x < 60 * 60 => format!("{} minutes", x / 60),
-        x if x >= 60 * 60 && x < 60 * 60 * 2 => "1 hour".into(),
-        x if x >= 60 * 60 * 2 && x < 60 * 60 * 24 => format!("{} hours", x / 60 / 60),
-        x if x >= 60 * 60 * 24 && x < 60 * 60 * 24 * 2 => "1 day".into(),
-        x if x >= 60 * 60 * 24 * 2 && x < S_IN_MNTH => {
+        x if (60..120).contains(&x) => "1 minute".into(),
+        x if (120..60 * 60).contains(&x) => format!("{} minutes", x / 60),
+        x if (60 * 60..60 * 60 * 2).contains(&x) => "1 hour".into(),
+        x if (60 * 60 * 2..60 * 60 * 24).contains(&x) => format!("{} hours", x / 60 / 60),
+        x if (60 * 60 * 24..60 * 60 * 24 * 2).contains(&x) => "1 day".into(),
+        x if (60 * 60 * 24 * 2..S_IN_MNTH).contains(&x) => {
             format!("{} days", x / 60 / 60 / 24)
         }
-        x if x >= S_IN_MNTH && x < 2 * S_IN_MNTH => "~1 month".into(),
-        x if x >= 2 * S_IN_MNTH && x < 12 * S_IN_MNTH => {
+        x if (S_IN_MNTH..2 * S_IN_MNTH).contains(&x) => "~1 month".into(),
+        x if (2 * S_IN_MNTH..12 * S_IN_MNTH).contains(&x) => {
             format!("~{} months", x / S_IN_MNTH)
         }
-        x if x >= 12 * S_IN_MNTH && x < 12 * 2 * S_IN_MNTH => "~1 year".into(),
+        x if (12 * S_IN_MNTH..12 * 2 * S_IN_MNTH).contains(&x) => "~1 year".into(),
         x => format!("~{} years", x / 12 / S_IN_MNTH),
     }
 }
