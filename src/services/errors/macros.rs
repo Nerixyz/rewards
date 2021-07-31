03e14c0df65d26958db3c8199f74f9bc1177fc33
Nerixyz <nero.9@hotmail.de>
2021-07-31 19:11:44 +0200

    feat: add discord log macro

--- src/services/errors/macros.rs
+++ src/services/errors/macros.rs
@@ -11,3 +11,31 @@ macro_rules! log_err {
             };
     };
 }
+
+#[macro_export]
+macro_rules! log_discord {
+    ($message:expr) => {
+        {if crate::config::CONFIG.log.webhook_url.is_some() {
+            let msg = crate::services::discord::WebhookReq::Content($message.into());
+            tokio::spawn(async move {
+                crate::log_err!(crate::services::discord::send_webhook_message(&msg).await, "Could not send webhook message");
+            });
+        };}
+    };
+    ($title:literal, $description:expr, $($name:literal = $value:expr),*) => {
+        crate::log_discord!($title, $description, 0xe91916, $($name = $value),*)
+    };
+    ($title:literal, $description:expr, $color:expr, $($name:literal = $value:expr),*) => {
+        {if crate::config::CONFIG.log.webhook_url.is_some() {
+            let msg = crate::services::discord::WebhookReq::Embeds(vec![crate::services::discord::Embed {
+                title: $title.into(),
+                description: $description.into(),
+                fields: vec![$(crate::services::discord::EmbedField::new($name, $value),)*],
+                color: (($color) as u32),
+            }]);
+            tokio::spawn(async move {
+                crate::log_err!(crate::services::discord::send_webhook_message(&msg).await, "Could not send webhook message");
+            });
+        };}
+    };
+}
