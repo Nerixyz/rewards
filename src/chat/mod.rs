11395f1d49e558c0c3abd4b3fa7001f41c12924c
Nerixyz <nero.9@hotmail.de>
2021-08-03 14:02:20 +0200

    fix: make `::about` reply to `::bot`

--- src/chat/mod.rs
+++ src/chat/mod.rs
@@ -14,7 +14,7 @@ pub fn try_parse_command(
 ) -> Option<AnyResult<Box<dyn ChatCommand + Send>>> {
     let cmd = match command.to_lowercase().as_str() {
         "ping" | "bing" => Ping::parse(args),
-        "about" | "rewardmore" | "who" => About::parse(args),
+        "about" | "rewardmore" | "who" | "bot" => About::parse(args),
         "emote" | "emoteinfo" | "ei" => EmoteInfo::parse(args),
         "slots" | "emoteslots" => SlotsCommand::parse(args),
         "emotes" | "currentemotes" | "ce" => Emotes::parse(args),
