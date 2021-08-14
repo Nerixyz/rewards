e90704f8cdf0253ccf70c862ccb682545e24197d
Nerixyz <nero.9@hotmail.de>
2021-08-14 16:05:07 +0200

    feat: add CRUD for discord settings

--- src/models/discord.rs
+++ src/models/discord.rs
@@ -1,5 +1,29 @@
 use errors::sql::SqlResult;
-use sqlx::PgPool;
+use serde::Serialize;
+use sqlx::{FromRow, PgPool};
+
+#[derive(Serialize, FromRow)]
+pub struct DiscordUserSettings {
+    pub user_id: String,
+    pub url: String,
+    pub log_emotes: bool,
+}
+
+pub async fn get_discord_settings(
+    user_id: &str,
+    pool: &PgPool,
+) -> SqlResult<Option<DiscordUserSettings>> {
+    // language=PostgreSQL
+    let settings = sqlx::query_as!(
+        DiscordUserSettings,
+        "SELECT * FROM discord_settings WHERE user_id = $1",
+        user_id
+    )
+    .fetch_optional(pool)
+    .await?;
+
+    Ok(settings)
+}
 
 pub async fn get_discord_webhook_url(user_id: &str, pool: &PgPool) -> SqlResult<Option<String>> {
     // language=PostgreSQL
@@ -11,4 +35,25 @@ pub async fn get_discord_webhook_url(user_id: &str, pool: &PgPool) -> SqlResult<
     .await?;
 
     Ok(url)
+}
+
+pub async fn set_discord_webhook_url(user_id: &str, url: &str, pool: &PgPool) -> SqlResult<()> {
+    // language=PostgreSQL
+    sqlx::query_scalar!(
+        "INSERT INTO discord_settings (user_id, url) VALUES ($1, $2) ON CONFLICT (user_id) DO UPDATE SET url = excluded.url",
+        user_id, url
+    )
+        .execute(pool)
+        .await?;
+
+    Ok(())
+}
+
+pub async fn delete_discord_webhook_url(user_id: &str, pool: &PgPool) -> SqlResult<()> {
+    // language=PostgreSQL
+    sqlx::query_scalar!("DELETE FROM discord_settings WHERE user_id = $1", user_id)
+        .execute(pool)
+        .await?;
+
+    Ok(())
 }
--- src/repositories/logs.rs
+++ src/repositories/logs.rs
@@ -1,12 +1,13 @@
 use crate::{
-    models::log_entry::LogEntry,
+    models::{discord, log_entry::LogEntry},
     services::{jwt::JwtClaims, sql::get_user_or_editor},
 };
 use actix_web::{
-    get,
+    delete, get, patch,
     web::{self, ServiceConfig},
     HttpResponse, Result,
 };
+use serde::Deserialize;
 use sqlx::PgPool;
 
 #[get("/{target_id}")]
@@ -20,6 +21,55 @@ async fn get_logs(
     Ok(HttpResponse::Ok().json(LogEntry::get_for_user(&user.id, &pool).await?))
 }
 
+#[derive(Deserialize)]
+struct SetUrlBody {
+    url: String,
+}
+
+#[patch("/{target_id}/discord")]
+async fn set_discord_url(
+    claims: JwtClaims,
+    target_id: web::Path<String>,
+    body: web::Json<SetUrlBody>,
+    pool: web::Data<PgPool>,
+) -> Result<HttpResponse> {
+    let user = get_user_or_editor(&claims, &target_id, &pool).await?;
+
+    discord::set_discord_webhook_url(&user.id, &body.url, &pool).await?;
+
+    Ok(HttpResponse::NoContent().finish())
+}
+
+#[get("/{target_id}/discord")]
+async fn get_discord_settings(
+    claims: JwtClaims,
+    target_id: web::Path<String>,
+    pool: web::Data<PgPool>,
+) -> Result<HttpResponse> {
+    let user = get_user_or_editor(&claims, &target_id, &pool).await?;
+
+    let settings = discord::get_discord_settings(&user.id, &pool).await?;
+
+    Ok(HttpResponse::Ok().json(settings))
+}
+
+#[delete("/{target_id}/discord")]
+async fn delete_discord_url(
+    claims: JwtClaims,
+    target_id: web::Path<String>,
+    pool: web::Data<PgPool>,
+) -> Result<HttpResponse> {
+    let user = get_user_or_editor(&claims, &target_id, &pool).await?;
+
+    discord::delete_discord_webhook_url(&user.id, &pool).await?;
+
+    Ok(HttpResponse::NoContent().finish())
+}
+
 pub fn init_log_routes(config: &mut ServiceConfig) {
-    config.service(get_logs);
+    config
+        .service(get_logs)
+        .service(get_discord_settings)
+        .service(set_discord_url)
+        .service(delete_discord_url);
 }
