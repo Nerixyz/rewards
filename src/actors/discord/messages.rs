ea16a020b9cc89b2d4f17469aae3de41f1891790
Nerixyz <nerixdev@outlook.de>
2024-03-19 16:51:41 +0100

    chore: run cargo-update

--- src/actors/discord/messages.rs
+++ src/actors/discord/messages.rs
@@ -2,7 +2,7 @@ use crate::services::discord::Embed;
 use actix::Message;
 
 #[derive(Message)]
-#[rtype("()")]
+#[rtype(result = "()")]
 pub struct LogToDiscordMessage {
     pub user_id: String,
     pub embed: Embed,
