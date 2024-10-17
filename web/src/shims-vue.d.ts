4698810ab95af0b36edd32717cf7f0389f1171be
Nerixyz <nerixdev@outlook.de>
2024-10-17 14:58:11 +0200

    refactor: migrate to new eslint

--- web/src/shims-vue.d.ts
+++ web/src/shims-vue.d.ts
@@ -1,6 +1,6 @@
 declare module '*.vue' {
   import { DefineComponent } from 'vue';
-  // eslint-disable-next-line @typescript-eslint/ban-types
+  // eslint-disable-next-line @typescript-eslint/no-empty-object-type
   const component: DefineComponent<{}, {}, unknown>;
   export default component;
 }
