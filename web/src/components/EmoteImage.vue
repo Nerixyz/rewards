870d1381593e3a1dc92b31dbf0be5f0dc6368861
Nerixyz <nerixdev@outlook.de>
2026-01-27 19:57:05 +0100

    fix: add link to image

--- web/src/components/EmoteImage.vue
+++ web/src/components/EmoteImage.vue
@@ -1,12 +1,12 @@
 <template>
-  <img :src="url" loading="lazy" class="max-w-10 h-10 object-contain m-auto" />
+  <a :href="url" target="_blank"><img :src="imageUrl" loading="lazy" class="max-w-10 h-10 object-contain m-auto" /></a>
 </template>
 <script lang="ts" setup>
 import { computed } from 'vue';
 import { SlotPlatform } from '../api/types';
 
 const props = defineProps<{ platform: SlotPlatform; id: string }>();
-const url = computed(() => {
+const imageUrl = computed(() => {
   switch (props.platform) {
     case 'Bttv':
       return `https://cdn.betterttv.net/emote/${encodeURIComponent(props.id)}/2x.webp`;
@@ -14,6 +14,17 @@ const url = computed(() => {
       return `https://cdn.frankerfacez.com/emote/${encodeURIComponent(props.id)}/2`;
     case 'SevenTv':
       return `https://cdn.7tv.app/emote/${encodeURIComponent(props.id)}/2x.webp`;
+  }
+  return '';
+});
+const url = computed(() => {
+  switch (props.platform) {
+    case 'Bttv':
+      return `https://betterttv.com/emotes/${encodeURIComponent(props.id)}`;
+    case 'Ffz':
+      return `https://www.frankerfacez.com/emoticon/${encodeURIComponent(props.id)}`;
+    case 'SevenTv':
+      return `https://7tv.app/emotes/${encodeURIComponent(props.id)}`;
   }
   return '';
 });
